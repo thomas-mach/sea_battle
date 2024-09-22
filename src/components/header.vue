5e544476b2a0c672b4b12a56cb53f8fa085e09ff
Tomasz Mach <m4chtomasz@gmail.com>
2024-09-22 17:24:12 +0200

    Add: layout

---
+++ src/components/header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="header">
+        <h1>header</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'header'
+}
+</script>
+
+<style scoped>
+.header {
+    width: 100%;
+    height: 15%;
+    background-color: lightblue;
+}
+</style>
---
+++ src/components/main.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main">
+        <h1>main</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'main'
+}
+</script>
+
+<style scoped>
+.main {
+    width: 100%;
+    height: 70%;
+    background-color: coral;
+}
+</style>
